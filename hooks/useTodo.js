eb1f7419b3f83a1cd001a1f5b6dc1b80815c4c70
Luca Luzzi <luzziluca6@gmail.com>
2022-10-03 19:26:51 -0300

    First commit

---
+++ hooks/useTodo.js
@@ -0,0 +1,54 @@
+import { useReducer, useEffect } from "react";
+import { todoReducer } from '../09-useReducer/todoReducer'
+
+const initialState = []
+
+const init = () => {
+    return JSON.parse( localStorage.getItem( 'todos' ) ) || [];
+}
+
+
+export const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
+
+    const todosCount = todos.length;
+    const pendingTodosCount = todos.filter( todo => todo.done !== true ).length;
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ))
+      }, [ todos ])
+  
+
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: 'Add todo',
+            payload: todo,
+        }
+
+        dispatch( action )
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: 'Delete todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: 'Toggle todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        todosCount,
+        pendingTodosCount,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+}
